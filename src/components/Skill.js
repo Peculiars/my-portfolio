96c3c4442bcabf67be181f3019df26a0b93f7bed
Peculiars <olaitandamilare230@gmail.com>
2024-04-25 21:04:16 +0100

    adding skills section

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { Skills } from './Skills';
 
 export const Skill = () => {
   return (
-    <div>Skill</div>
+    <>
+        <h2 className='font-bold text-8xl w-full text-center mt-64'>Skills</h2>
+        <div className='relative w-full h-screen flex items-center justify-center rounded-full bg-circularLight'>
+            <motion.div className='flex items-center justify-center bg-dark text-light font-semibold rounded-full shadow-dark p-5 cursor-pointer' whileHover={{scale: 1.1}}>
+                web
+            </motion.div>
+            <Skills name='CSS' x='-5vw' y='-10vw'/>
+        </div>
+    </>
   )
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+export const Skills = ({name, x, y}) => {
+    return(
+        <motion.div className='flex items-center justify-center bg-dark text-light font-semibold rounded-full shadow-dark py-3 px-6 absolute cursor-pointer' whileHover={{scale: 1.1}} initial={{x:0, y:0}} animate={{x:x, y:y}} transition={{duration:1.5}}>
+            {name}
+        </motion.div>
+    )
+}
