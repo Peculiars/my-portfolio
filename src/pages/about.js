0bcbe90dddac16a1c4f8662222b5282efca10c6a
Peculiars <olaitandamilare230@gmail.com>
2024-05-05 03:12:36 +0100

    updating build error

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,14 @@
-import { AnimatedText } from '@/components/AnimatedText'
-import { Layout } from '@/components/Layout'
-import Head from 'next/head'
-import Image from 'next/image'
+import { AnimatedText } from '@/components/AnimatedText';
+import { Layout } from '@/components/Layout';
+import Head from 'next/head';
+import Image from 'next/image';
 import dynasty from '../../public/images/profile/dynasty.png';
 import { useEffect, useRef } from 'react';
 import { useInView, useMotionValue, useSpring } from 'framer-motion';
 import { Skill } from '@/components/Skill';
 import { Experience } from '@/components/Experience';
 import { Certification } from '@/components/Certification';
-import { TransitionEffect } from '@/components/TransitionEffect'
+import { TransitionEffect } from '@/components/TransitionEffect';
 
 export default function About() {
 
@@ -16,11 +16,11 @@ export default function About() {
     const ref = useRef(null);
     const motionValue = useMotionValue(0);
     const springValue = useSpring(motionValue, {duration: 3000});
-    const isInView = useInView(ref)
+    const isInView = useInView(ref);
 
     useEffect(()=>{
       if(isInView){
-        motionValue.set(value)
+        motionValue.set(value);
       }
     },[isInView, value, motionValue]);
 
@@ -29,11 +29,11 @@ export default function About() {
         if(ref.current && latest.toFixed(0) <= value){
           ref.current.textContent = latest.toFixed(0);
         }
-      })
-    },[springValue, value])
+      });
+    },[springValue, value]);
 
-    return <span ref={ref}></span>
-  }
+    return <span ref={ref}></span>;
+  };
   return (
     <>
       <Head>
@@ -49,14 +49,14 @@ export default function About() {
           <div className='grid w-full grid-cols-8 xs:gap-y-14 xs:gap-x-0 md:gap-16 my-4'>
             <div className=' xl:col-span-3 lg:col-span-4 xs:col-span-8 xs:order-2 flex flex-col items-center justify-start'>
               <h2 className='text-lg font-bold text-dark dark:text-light uppercase mb-4'>Biography</h2>
-              <p className='font-medium my-2'>Hi, I'm Olaitan Damilare or you call me Dynasty, am a web and mobile developer with a passion for creating beautiful, functional, 
+              <p className='font-medium my-2'>Hi, I&apos;m Olaitan Damilare or you call me Dynasty, am a web and mobile developer with a passion for creating beautiful, functional, 
                 and user-centered digital experiences. With 2 years of experience in the field. I am always looking for 
-                new and innovative ways to bring my clients' visions to life.</p>
+                new and innovative ways to bring my clients&apos; visions to life.</p>
 
-              <p className='font-medium my-2'>I believe that developing digital presence is something more than just making the UI looks pretty alone, it's more about solving problems and 
+              <p className='font-medium my-2'>I believe that developing digital presence is something more than just making the UI looks pretty alone, it&apos;s more about solving problems and 
               creating intuitive, enjoyable experiences for users. </p>
 
-              <p className='font-medium my-2'>Whether I'm working on a website, mobile app, or 
+              <p className='font-medium my-2'>Whether I&apos;m working on a website, mobile app, or 
               other digital product, I bring my commitment to design excellence and user-centered thinking to 
               every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
             </div>
@@ -86,5 +86,5 @@ export default function About() {
         </Layout>
       </main>
     </>
-  )
+  );
 }
